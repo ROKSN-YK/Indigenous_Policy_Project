2721bba247b7fd178b7f00134f028f7817482d03
Ahn <ahn890209@gmail.com>
2026-05-11 11:27:02 +0800

    Update R code

--- code/00-00-data-cleaning.R
+++ code/00-00-data-cleaning.R
@@ -7,4 +7,4 @@ library(dplyr)
 dt <- read_dta('data/raw_data/AJ030005 110年原住民族經濟狀況調查/data110.dta') %>%
   mutate(across(where(is.labelled), as_factor)) %>% 
   select(id, level, county1)  # 在這裡放入你需要的欄位，我先隨意放入三個欄位
-  
+# this is a test
